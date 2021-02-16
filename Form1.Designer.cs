87c72acc7497fc587c7934993c8622d0fde64d2c
Almir-c <70958278+Almir-c@users.noreply.github.com>
2021-02-16 18:55:18 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButtonAlgebra = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxForm1 = new System.Windows.Forms.TextBox();
+            this.buttonForm1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(629, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // radioButtonAlgebra
+            // 
+            this.radioButtonAlgebra.AutoSize = true;
+            this.radioButtonAlgebra.Location = new System.Drawing.Point(635, 27);
+            this.radioButtonAlgebra.Name = "radioButtonAlgebra";
+            this.radioButtonAlgebra.Size = new System.Drawing.Size(83, 21);
+            this.radioButtonAlgebra.TabIndex = 1;
+            this.radioButtonAlgebra.TabStop = true;
+            this.radioButtonAlgebra.Text = "Алгебра";
+            this.radioButtonAlgebra.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(635, 54);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 21);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Физика";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(635, 81);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(118, 21);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Русский язык";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(635, 108);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(101, 21);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Геометрия";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(635, 135);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(141, 21);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Английский язык";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(638, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Я выбрал предмет!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxForm1
+            // 
+            this.textBoxForm1.Location = new System.Drawing.Point(794, 379);
+            this.textBoxForm1.Name = "textBoxForm1";
+            this.textBoxForm1.Size = new System.Drawing.Size(100, 22);
+            this.textBoxForm1.TabIndex = 9;
+            this.textBoxForm1.TextChanged += new System.EventHandler(this.textBoxForm1_TextChanged);
+            // 
+            // buttonForm1
+            // 
+            this.buttonForm1.Location = new System.Drawing.Point(819, 332);
+            this.buttonForm1.Name = "buttonForm1";
+            this.buttonForm1.Size = new System.Drawing.Size(75, 23);
+            this.buttonForm1.TabIndex = 10;
+            this.buttonForm1.Text = "Выбрать";
+            this.buttonForm1.UseVisualStyleBackColor = true;
+            this.buttonForm1.Click += new System.EventHandler(this.buttonForm1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(0, 292);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 235);
+            this.panel1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(906, 539);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonForm1);
+            this.Controls.Add(this.textBoxForm1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButtonAlgebra);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButtonAlgebra;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxForm1;
+        private System.Windows.Forms.Button buttonForm1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public struct Uchebniki
+    {
+        public PictureBox oblojka;
+        public PictureBox soderzanie;
+        public string Author;
+        public int YourIq; 
+    }
+
+    public partial class Form1 : Form
+    {
+        Uchebniki[] spisok = new Uchebniki[3];
+        public Form1()
+        {
+            InitializeComponent();
+
+            spisok[0].Author = "Петушков";
+            spisok[0].YourIq = 0;
+
+            spisok[1].Author = "Абрамов";
+            spisok[1].YourIq = -1;
+
+            spisok[2].Author = "Машкова";
+            spisok[2].YourIq = 100;
+
+            for (int i = 0; i < 3; i++)
+            {
+                spisok[i].oblojka = new PictureBox();
+                spisok[i].soderzanie = new PictureBox();
+
+                try
+                {
+                    spisok[i].oblojka.Load("../../Resources/" + spisok[i].Author + ".jpg");
+                    spisok[i].soderzanie.Load("../../Resources/" + spisok[i].Author + " (2).jpg");
+                }
+                catch (Exception)
+                {
+                    spisok[i].oblojka.Load("../../Resources/какава.png");
+                    spisok[i].soderzanie.Load("../../Resources/какава.png");
+                }
+
+                spisok[i].oblojka.Location = new Point(150 * i + 10, 10);
+                spisok[i].oblojka.Size = new Size(120, 138);
+                spisok[i].oblojka.SizeMode = PictureBoxSizeMode.Zoom;
+
+                spisok[i].soderzanie.Location = new Point(150 * i + 10, 150);
+                spisok[i].soderzanie.Size = new Size(120, 138);
+                spisok[i].soderzanie.SizeMode = PictureBoxSizeMode.Zoom;
+
+                panel1.Controls.Add(spisok[i].oblojka);
+                panel1.Controls.Add(spisok[i].soderzanie);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButtonAlgebra.Checked)
+            {
+                PredmetForm f = new PredmetForm("Алгебра");
+                f.Show();
+            }
+            if (radioButton2.Checked)
+            {
+                PredmetForm f = new PredmetForm("Физика");
+                f.Show();
+            }
+            if (radioButton3.Checked)
+            {
+                PredmetForm f = new PredmetForm("Русский язык");
+                f.Show();
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Load("../../Resources/гдз.png");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxForm1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void buttonForm1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++) 
+            {
+                spisok[i].oblojka.Visible = true;
+                spisok[i].soderzanie.Visible = true;
+                if (!spisok[i].Author.Contains(textBoxForm1.Text))
+                {
+                    spisok[i].oblojka.Visible = false;
+                    spisok[i].soderzanie.Visible = false;
+                }
+            }
+        }
+    }
+}
