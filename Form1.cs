8c43dc986cdbe40a8d884f62fc9ced1942096888
Almir-c <70958278+Almir-c@users.noreply.github.com>
2021-03-02 18:48:10 +0400

    я хочу пиццу

--- Form1.cs
+++ Form1.cs
@@ -14,52 +14,80 @@ namespace WindowsFormsApp3
     {
         public PictureBox oblojka;
         public PictureBox soderzanie;
+        public PictureBox soderzanie2;
         public string Author;
         public int YourIq; 
+
+        public Uchebniki(string _Author, int _YourIq)
+        {
+            Author = _Author;
+            YourIq = _YourIq;
+            oblojka = new PictureBox();
+            soderzanie = new PictureBox();
+            soderzanie2 = new PictureBox();
+        }
     }
 
     public partial class Form1 : Form
     {
-        Uchebniki[] spisok = new Uchebniki[3];
+        List<Uchebniki> spisok = new List<Uchebniki>();
+        //Uchebniki[] spisok = new Uchebniki[300];
         public Form1()
         {
             InitializeComponent();
 
-            spisok[0].Author = "Петушков";
-            spisok[0].YourIq = 0;
-
-            spisok[1].Author = "Абрамов";
-            spisok[1].YourIq = -1;
-
-            spisok[2].Author = "Машкова";
-            spisok[2].YourIq = 100;
-
-            for (int i = 0; i < 3; i++)
+            spisok.Add(new Uchebniki ("Петушков", 0));
+            
+            spisok.Add(new Uchebniki("Абрамов", -1));
+            spisok.Add(new Uchebniki("Машкова", 100));
+            spisok.Add(new Uchebniki("Владимиров", 50));
+            spisok.Add(new Uchebniki("Anonim", 1000));
+            spisok.Add(new Uchebniki("Верблюдов", 25));
+            
+            int x = 10;
+            int y = 20;
+            for (int i = 0; i < spisok.Count; i++)
             {
-                spisok[i].oblojka = new PictureBox();
-                spisok[i].soderzanie = new PictureBox();
-
                 try
                 {
                     spisok[i].oblojka.Load("../../Resources/" + spisok[i].Author + ".jpg");
-                    spisok[i].soderzanie.Load("../../Resources/" + spisok[i].Author + " (2).jpg");
                 }
                 catch (Exception)
                 {
                     spisok[i].oblojka.Load("../../Resources/какава.png");
+                }                                                                                          
+                try
+                {
+                    spisok[i].soderzanie.Load("../../Resources/" + spisok[i].Author + " (2).jpg");
+                    spisok[i].soderzanie2.Load("../../Resources/" + spisok[i].Author + " (3).jpg");
+                }
+                catch (Exception)
+                {
                     spisok[i].soderzanie.Load("../../Resources/какава.png");
                 }
 
-                spisok[i].oblojka.Location = new Point(150 * i + 10, 10);
+                spisok[i].oblojka.Location = new Point(x, 10);
                 spisok[i].oblojka.Size = new Size(120, 138);
                 spisok[i].oblojka.SizeMode = PictureBoxSizeMode.Zoom;
 
-                spisok[i].soderzanie.Location = new Point(150 * i + 10, 150);
+                spisok[i].soderzanie.Location = new Point(x, 150);
                 spisok[i].soderzanie.Size = new Size(120, 138);
                 spisok[i].soderzanie.SizeMode = PictureBoxSizeMode.Zoom;
 
+                spisok[i].soderzanie2.Location = new Point(x, 150);
+                spisok[i].soderzanie2.Size = new Size(120, 138);
+                spisok[i].soderzanie2.SizeMode = PictureBoxSizeMode.Zoom;
+
                 panel1.Controls.Add(spisok[i].oblojka);
                 panel1.Controls.Add(spisok[i].soderzanie);
+                panel1.Controls.Add(spisok[i].soderzanie2);
+
+                x = x + 150;
+                if (x  + 120 > Width)
+                {
+                    y = y + 180;
+                    x = 10;
+                }
             }
         }
 
@@ -100,14 +128,16 @@ namespace WindowsFormsApp3
 
         private void buttonForm1_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < 3; i++) 
+            for (int i = 0; i < spisok.Count; i++) 
             {
                 spisok[i].oblojka.Visible = true;
                 spisok[i].soderzanie.Visible = true;
+                spisok[i].soderzanie2.Visible = true;
                 if (!spisok[i].Author.Contains(textBoxForm1.Text))
                 {
                     spisok[i].oblojka.Visible = false;
                     spisok[i].soderzanie.Visible = false;
+                    spisok[i].soderzanie2.Visible = false;
                 }
             }
         }
