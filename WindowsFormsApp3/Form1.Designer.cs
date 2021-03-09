d28d5c87b60ca8eda9a0021c461e87d732fc64a7
mabramovsd <beavisabra@yandex.ru>
2021-03-09 17:39:51 +0400

    Hello

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -46,7 +46,7 @@ namespace WindowsFormsApp3
             // 
             this.pictureBox1.Location = new System.Drawing.Point(0, -2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(629, 266);
+            this.pictureBox1.Size = new System.Drawing.Size(629, 248);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
